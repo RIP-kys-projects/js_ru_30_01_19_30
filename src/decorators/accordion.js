663d5c44aa8d87e3f1b5101c97d4f46e016530ba
Karalkou <yury.karalkou@gmail.com>
2017-02-11 10:21:25 +0300

    Add small renaming in accordion-decorator ( branch: hw3 )

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -1,12 +1,12 @@
 import React, {Component as ReactComponent}  from 'react'
 
-export default (Component) => class Accordion extends ReactComponent {
+export default (MyComponent) => class Accordion extends ReactComponent {
     state = {
         openItemId: null
     }
 
     render() {
-        return <Component {...this.props} isOpenItem = {this.isOpenItem} toggleOpenItem = {this.toggleOpenItem}/>
+        return <MyComponent {...this.props} isOpenItem = {this.isOpenItem} toggleOpenItem = {this.toggleOpenItem}/>
     }
 
     // чтобы понять, откуда id, смотри в ArticleList.js
