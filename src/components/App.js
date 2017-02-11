4f1b5ab4b667dfb76a9647df2e617981f560b3bc
Karalkou <yury.karalkou@gmail.com>
2017-02-11 22:59:21 +0300

    Add result of HT 3.2 without understanding ( branch: hw3 )

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,41 @@
 import React, { PropTypes, Component } from 'react'
 import ArticleList from './ArticleList'
+
 import Chart from './Chart'
+
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
 import '../styles/react-select_custom.css'
 
+import DayPickerComponent from "./DayPickerComponent/DayPickerComponent";
+import './DayPickerComponent/DayPickerComponent.css'
+
 class App extends Component {
     state = {
         user: '',
-        selection: null
-    }
+        selection: null,
+        selectedDay: new Date(),
+    };
 
     render() {
-        const {articles} = this.props
+        const {articles} = this.props;
         const options = articles.map(article => ({
             label: article.title,
             value: article.id
-        }))
+        }));
+
         return (
             <div>
                 User: <input type="text" value={this.state.user} onChange={this.handleUserChange}/>
                 <Select options = {options} onChange={this.handleSelectChange} value={this.state.selection} multi/>
-                <ArticleList articles={articles}/>
+                <DayPickerComponent />
+                <ArticleList articles={articles} />
                 <Chart articles={articles}/>
             </div>
         )
     }
 
-    handleSelectChange = selection => this.setState({ selection })
+    handleSelectChange = selection => this.setState({ selection });
 
     handleUserChange = (ev) => {
         if (ev.target.value.length < 10) {
@@ -35,11 +43,11 @@ class App extends Component {
                 user: ev.target.value
             })
         }
-    }
+    };
 }
 
 App.propTypes = {
     articles: PropTypes.array.isRequired
-}
+};
 
 export default App
---
+++ src/components/DayPickerComponent/DayPickerComponent.js
@@ -0,0 +1,66 @@
+import React, { PropTypes, Component } from 'react'
+
+import moment from 'moment';
+import DayPicker, { DateUtils } from "react-day-picker";
+import 'react-day-picker/dist/style.css'
+
+export default class DayPickerComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDayClick = this.handleDayClick.bind(this);
+        this.handleResetClick = this.handleResetClick.bind(this);
+    }
+    state = {
+        from: null,
+        to: null,
+    };
+    handleDayClick(e, day) {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    }
+    handleResetClick(e) {
+        e.preventDefault();
+        this.setState({
+            from: null,
+            to: null,
+        });
+    }
+    /*state = {
+        selectedDay: new Date(),
+    };*/
+
+    render() {
+        const { from, to } = this.state;
+
+        const MONTHS = ['Студзень', 'Люты', 'Сакавік', 'Красавік', 'Травень',
+            'Чэрвень', 'Ліпень', 'Жнівень', 'Верасень', 'Кастрычнік', 'Лістапад',
+            'Снежань'];
+        const WEEKDAYS_LONG = ['Нядзеля', 'Панядзелак', 'Аўторак', 'Серада', 'Чацвер', 'Пятніца', 'Субота'];
+        const WEEKDAYS_SHORT = ['Нд', 'Пн', 'Ат', 'Ср', 'Чц', 'Пт', 'Сб' ];
+
+        return (
+            <div>
+                { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
+                { from && !to && <p>Please select the <strong>last day</strong>.</p> }
+                { from && to &&
+                    <p>
+                        You chose from { moment(from).format('L') } to { moment(to).format('L') }.
+                        { ' ' }<a href="." onClick={ this.handleResetClick }>Reset</a>
+                    </p>
+                }
+                <DayPicker
+                    locale="by"
+                    months={ MONTHS }
+                    weekdaysLong={ WEEKDAYS_LONG }
+                    weekdaysShort={ WEEKDAYS_SHORT }
+                    firstDayOfWeek={ 1 }
+
+                    initialMonth={ new Date(2017, 1) }
+                    selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                    onDayClick={ this.handleDayClick }
+                />
+            </div>
+        )
+    }
+}
