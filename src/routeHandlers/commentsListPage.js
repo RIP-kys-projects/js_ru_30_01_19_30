029557b0080b7e7970befefb845301efe05ba967
Karalkou <yury.karalkou@gmail.com>
2017-03-04 13:17:19 +0300

    Start to create comments router ( branch hw7 )

---
+++ src/routeHandlers/commentsListPage.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react'
+
+class commentsListPage extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <div>CommentsList title</div>
+                <div>comment from nested route</div>
+                <div>pagination line</div>
+            </div>
+        )
+    }
+}
+
+export default commentsListPage
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import Counter from './routeHandlers/Counter'
 import ArticlePage from './routeHandlers/ArticlePage'
 import NotFoundPage from './routeHandlers/NotFoundPage'
 import AuthorizedOnlyPage from './routeHandlers/AuthorizedOnlyPage'
+import CommentsListPage from './routeHandlers/CommentsListPage'
 import ErrorPage from './routeHandlers/ErrorPage'
 import store from './store'
 
@@ -14,20 +15,24 @@ export default (
     <Router history={browserHistory}>
         <Route path="/" component={Root}>
             <Route path="articles" component={ArticleList}>
-                <Route path=":id" component={ArticlePage} />
+                <Route path=":id" component={ArticlePage}/>
             </Route>
             <Route path="filters" component={Filters}/>
             <Route path="counter" component={Counter}/>
             <Route path="admin" component={AuthorizedOnlyPage}
-                onEnter={(routeState, replace) => {
-                    if (!store.getState().user) {
-                        replace('/error')
-                    }
-                    }
-                }
+                   onEnter={
+                       (routeState, replace) => {
+                           if (!store.getState().user) {
+                               replace('/error')
+                           }
+                       }
+                   }
             />
-            <Route path="error" component={ErrorPage} />
-            <Route path="*" component={NotFoundPage} />
+            <Route path="comments" component={CommentsListPage}>
+                {/*<Route path=":id" component={ArticlePage}/>*/}
+            </Route>
+            <Route path="error" component={ErrorPage}/>
+            <Route path="*" component={NotFoundPage}/>
         </Route>
     </Router>
 )
